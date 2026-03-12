56d41d2f3bf95e37409880be84044fe7cba95150
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-12 19:27:29 +0700

    code for insertion and pause

--- insertionosrt.cpp
+++ insertionosrt.cpp
@@ -33,6 +33,25 @@ void input()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" << endl;
 
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+int main()
+{
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
 
     
