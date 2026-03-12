307a716ea2e770b08b10b01ab6cc525f25024cdb
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-12 19:19:37 +0700

    code untuk menjalankan perulangan

--- insertionosrt.cpp
+++ insertionosrt.cpp
@@ -7,5 +7,19 @@ int n;
 void input()
 {
     while (true)
+    {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
     
 }
+    
